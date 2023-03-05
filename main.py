df522b159ff83cf4f5fcd340d8d0e2dbc9026f54
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-03-05 01:17:18 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import streamlit as st
+import glob
+from nltk.sentiment import SentimentIntensityAnalyzer
+import plotly.express as px
+
+filenames = glob.glob('diary/*.txt')
+
+entries = []
+for filename in filenames:
+    with open(filename, 'r') as file:
+        filename = filename.split('\\')[1]
+        date = filename.split('.')[0]
+        entries.append({"date": date, "entry": file.read()})
+
+analyzer = SentimentIntensityAnalyzer()
+
+analyses = []
+for entry in entries:
+    analysis = analyzer.polarity_scores((entry['entry']))
+    analysis['date'] = entry['date']
+    analyses.append(analysis)
+
+df = pd.DataFrame(analyses)
+print(df.dtypes)
+pos = px.line(x=df['date'], y=df['pos'], labels={"x": "date", "y": "positivity rating"})
+neg = px.line(x=df['date'], y=df['neg'], labels={"x": "date", "y": "negativity rating"})
+
+st.title("Diary Analysis")
+
+st.subheader('Positive')
+st.plotly_chart(pos)
+
+st.subheader('Negative')
+st.plotly_chart(neg)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# What is this project?
+It's a data app to analyse the sentiment of each diary entry and present it in a chart
